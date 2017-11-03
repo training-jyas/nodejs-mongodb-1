029344a055fe6193f54fe09a1863093ef9cb39ae
Jyasveer Gotta <jyasveer@gmail.com>
2017-11-03 11:25:39 +0530

    nodes js introduction and require

---
+++ 02-require-own-files/app.js
@@ -0,0 +1,15 @@
+console.log('Starting app.js');
+
+const fs = require('fs');
+const os = require('os');
+
+const notes = require('./notes.js');
+console.log('notes', notes);
+console.log(notes.addNote());
+console.log('Result =', notes.add(9, -2));
+// console.log('Result:', notes.sub(9, -2));
+
+var user = os.userInfo();
+console.log('user', user);
+
+fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
